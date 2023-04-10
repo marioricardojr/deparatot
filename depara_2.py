6a1b9db11e5cf8d9090554a3a97420628aae8bfe
marioricardojr <38199552+marioricardojr@users.noreply.github.com>
2023-04-10 20:29:56 -0300

    Add files via upload

--- depara_2.py
+++ depara_2.py
@@ -148,34 +148,57 @@ def estrutura_dados(id):
     return norma, atributos, analise_id
 
 
-def verifica_conformidade(norma,analise):
+def verifica_conformidade(norma,analise,n):
     atributos_ok=0
     for p in range(0, len(analise)):
         for q in range(0, len(norma)):
             total_atributos = len(analise)
+
             norma[q][0] = re.sub(r"\s+", "", norma[q][0], flags=re.UNICODE)
             analise[p][0] = re.sub(r"\s+", "", analise[p][0], flags=re.UNICODE)
-            result_compara = ngram.NGram.compare(((analise[p][0]).lower()).replace(" ", ""),
-                                                 ((norma[q][0]).lower()).replace(" ", ""), N=1)
-            # result_compara = (jellyfish.jaro_distance(str(norma[q][0]), analise[p][0]))
-            #print("a comparação entre",analise[p][0],"e",norma[q][0],"eh",result_compara,"\n")
-            # matching perfeito entre os dados
-            if result_compara == 1:
-                #print("compara",analise[p][0],"e",norma[q][0])
-                # faz as operaçoes, nesse caso só está implementado operação menor igual  #1, maior igual #2, intervalo #3
-                if norma[p][3] == 1:
-                    if (analise[p][1] >= norma[q][1]):
-                        atributos_ok = atributos_ok+1
+
+            if n==1:
+
+                if (((analise[p][0]).lower()).replace(" ", ""))==(((norma[q][0]).lower()).replace(" ", "")):
+
+                    if norma[p][3] == 1:
+                        if (analise[p][1] >= norma[q][1]):
+                            atributos_ok = atributos_ok+1
 
 
-                if norma[p][3] == 2:
-                    if (analise[p][1] <= norma[q][2]):
-                        atributos_ok = atributos_ok+1
+                    if norma[p][3] == 2:
+                        if (analise[p][1] <= norma[q][2]):
+                            atributos_ok = atributos_ok+1
 
 
-                if norma[p][3] == 3:
-                    if ((analise[p][1] >= norma[q][1])and(analise[p][1] <= norma[q][2])):
-                        atributos_ok = atributos_ok+1
+                    if norma[p][3] == 3:
+                        if ((analise[p][1] >= norma[q][1])and(analise[p][1] <= norma[q][2])):
+                            atributos_ok = atributos_ok+1
+
+
+            if n<1:
+                result_compara = ngram.NGram.compare(((analise[p][0]).lower()).replace(" ", ""),
+                                                     ((norma[q][0]).lower()).replace(" ", ""), N=1)
+                # result_compara = (jellyfish.jaro_distance(str(norma[q][0]), analise[p][0]))
+                #print("a comparação entre",analise[p][0],"e",norma[q][0],"eh",result_compara,"\n")
+            # Se o valor encontrado pelo ngrama é maior que o minimo aceitavel definido
+                if result_compara >=n:
+                    #print("compara",analise[p][0],"e",norma[q][0])
+                    # faz as operaçoes, nesse caso só está implementado operação menor igual  #1, maior igual #2, intervalo #3
+                    if norma[p][3] == 1:
+                        if (analise[p][1] >= norma[q][1]):
+                            atributos_ok = atributos_ok+1
+
+
+                    if norma[p][3] == 2:
+                        if (analise[p][1] <= norma[q][2]):
+                            atributos_ok = atributos_ok+1
+
+
+                    if norma[p][3] == 3:
+                        if ((analise[p][1] >= norma[q][1])and(analise[p][1] <= norma[q][2])):
+                            atributos_ok = atributos_ok+1
+
 
 
         percentual = (atributos_ok / total_atributos) * 100
@@ -191,7 +214,9 @@ id_api=1
 norma_analisada,doc_analisado,analiseid = estrutura_dados(id_api)
 
 #calcula conformidade
-resultado_conformidade = verifica_conformidade(norma_analisada,doc_analisado)
+
+n=1
+resultado_conformidade = verifica_conformidade(norma_analisada,doc_analisado,n)
 
 #atualiza o valor da conformidade no banco
 update("analise","PercentualConfor",resultado_conformidade,analiseid)
